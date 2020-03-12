14ad8bbdbc7af3f5a12afd8d41c80c40600c37bc
mentikora <notender_boy@ukr.net>
2020-03-12 13:08:23 +0200

    welcome section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Welcome } from 'layout';
+import './scss/app.scss';
 
 function App() {
   return (
--- src/layout/welcome/welcome.tsx
+++ src/layout/welcome/welcome.tsx
@@ -1,6 +1,69 @@
 import React from 'react';
-import styles from './Welcome.module.scss';
+import {
+  Box,
+  Typography,
+} from '@material-ui/core';
+import css from './Welcome.module.scss';
+import img from './images/fish.png';
 
-export const Welcome = () => <div className={styles.container}>
-  <p>welcome</p>
-</div>;
+const Logo = () => {
+  return (
+    <Box component="div" className={css.logo} flexGrow={1}>
+      <Typography
+        component="h1"
+        variant="h1"
+        classes={{root: 'logo-heading'}}
+        className={css.logo__pre}
+      >
+        Ryoshi
+      </Typography>
+      <img className={css.logo__image} src={img} alt="Ryoshi" />
+      <Typography
+        component="h2"
+        variant="h1"
+        classes={{root: 'logo-heading'}}
+        className={css.logo__post}
+      >
+        Ryoshi
+      </Typography>
+    </Box>
+  )
+};
+
+export const Welcome = () => <Box className={css.container}>
+  <Box className={css.indents}>
+    <Box
+      component="div"
+      className={css.outer}
+      boxShadow={10}
+    >
+      <Box
+        className={css.inner}
+        display="flex"
+        flexDirection="column"
+        flexGrow={1}
+      >
+        <Box
+          display="flex"
+          flexDirection="column"
+          flexGrow={1}
+        >
+          <Box component="header">
+            header
+          </Box>
+          <Box
+            component="div"
+            display="flex"
+            alignItems="center"
+            flexGrow={1}
+          >
+            <Logo />
+          </Box>
+          <Box component="footer">
+            footer
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  </Box>
+</Box>;
