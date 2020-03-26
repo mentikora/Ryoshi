fea8567a5950d5fed6622f34b0ab685130f61c5d
mentikora <notender_boy@ukr.net>
2020-03-26 13:24:54 +0200

    change language

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,10 +4,15 @@ import { useTranslation } from 'react-i18next';
 
 export const Header = () => {
   const { t, i18n } = useTranslation();
+
+  const changeLanguage = lang => {
+    i18n.changeLanguage(lang);
+  }
+
   return (
     <Box component="header">
-      <Button size="small">{t('button.lang.ua')}</Button>
-      <Button size="small">{t('button.lang.en')}</Button>
+      <Button size="small" onClick={() => changeLanguage('ua')}>{t('button.lang.ua')}</Button>
+      <Button size="small" onClick={() => changeLanguage('en')}>{t('button.lang.en')}</Button>
       <div className="auth">
         <Button>{t('button.signUp')}</Button>
         <Button>{t('button.signIn')}</Button>
