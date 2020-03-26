43cbe8aef673ed378ad4b75e12ee13034238ec27
mentikora <notender_boy@ukr.net>
2020-03-26 12:47:22 +0200

    header footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,3 +1,10 @@
 import React from 'react';
+import { Box, Typography } from '@material-ui/core';
 
-export const Footer = () => <div>footer</div>;
+export const Footer = () => {
+  return (
+    <Box component="footer" p={2}>
+      <Typography component="p">&copy; Ryoshi 2020</Typography>
+    </Box>
+  )
+};
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,3 +1,17 @@
 import React from 'react';
+import { Box, Button } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
 
-export const Header = () => <div>header</div>;
+export const Header = () => {
+  const { t, i18n } = useTranslation();
+  return (
+    <Box component="header">
+      <Button size="small">{t('button.lang.ua')}</Button>
+      <Button size="small">{t('button.lang.en')}</Button>
+      <div className="auth">
+        <Button>{t('button.signUp')}</Button>
+        <Button>{t('button.signIn')}</Button>
+      </div>
+    </Box>
+  )
+};
